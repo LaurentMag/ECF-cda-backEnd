583be63c6bbefab4a6fba2413cc85371554eb870
Laurent <magrez0laurent@gmail.com>
2022-12-08 11:08:42 +0100

    doc: controller location

--- src/main/java/fr/formation/ecf/backend/ecf3cdabackend/locations/LocationController.java
+++ src/main/java/fr/formation/ecf/backend/ecf3cdabackend/locations/LocationController.java
@@ -15,21 +15,39 @@ public class LocationController {
         this.locationService = locationService;
     }
 
+    /**
+     * Récupère la liste des locations de la database sur la route /locations
+     * @return liste des locations
+     */
     @GetMapping("")
     public List<Location> findAll() {
         return locationService.findAll();
     }
 
+    /**
+     * Récupère un location par son ID sur la route /locations/id
+     * @param id de la location en PathVariable
+     * @return la location trouvée ( ou erreur envoyé par le service )
+     */
     @GetMapping("{id}")
     public Location findById(@PathVariable String id) {
         return locationService.findById(id);
     }
 
+    /**
+     * Ajout d'une location sur la base de donnée, utilisant la request de chemin /locations
+     * @param location L'object location envoyé dans le body de la request
+     * @return la location sauvegardée
+     */
     @PostMapping("")
     public Location save(@RequestBody Location location) {
         return locationService.save(location);
     }
 
+    /**
+     * Suppression d'une location de la base de donnée sur le chemin /locations/id
+     * @param id valeur de l'id envoyé en PathVariable
+     */
     @DeleteMapping("{id}")
     public void deleteById(@PathVariable String id) {
         locationService.deleteById(id);
