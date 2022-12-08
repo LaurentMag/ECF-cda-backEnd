1a12bcb959cd3540a04232ded5f2e17342c03053
Laurent <magrez0laurent@gmail.com>
2022-12-08 11:06:40 +0100

    doc: controller client

--- src/main/java/fr/formation/ecf/backend/ecf3cdabackend/clients/ClientController.java
+++ src/main/java/fr/formation/ecf/backend/ecf3cdabackend/clients/ClientController.java
@@ -10,17 +10,28 @@ import java.util.List;
 public class ClientController {
     private final ClientService clientService;
 
+
     public ClientController(ClientService clientService) {
         this.clientService = clientService;
     }
 
     //========================================================================
     //GET
+    /**
+     * Récupère la liste des clients de la database sur la route /clients
+     * @return liste des clients
+     */
+
     @GetMapping("")
     public List<Client> findAll() {
         return clientService.findAll();
     }
 
+    /**
+     * Récupère un client par son ID sur la route /clients/id
+     * @param id du client en PathVariable
+     * @return le client trouvé ( ou erreur envoyé par le service )
+     */
     @GetMapping("{id}")
     public Client findById(@PathVariable String id) {
         return clientService.findById(id);
@@ -28,6 +39,11 @@ public class ClientController {
 
     //========================================================================
     // POST
+    /**
+     * Ajout d'un client sur la base de donnée, utilisant la request de chemin /clients
+     * @param client L'object client envoyé dans le body de la request
+     * @return le client sauvegardé
+     */
     @PostMapping("")
     public Client save(@RequestBody Client client) {
         return clientService.save(client);
@@ -35,6 +51,10 @@ public class ClientController {
 
     //========================================================================
     //DELETE
+    /**
+     * Suppression d'un client de la base de donnée sur le chemin /clients/id
+     * @param id valeur de l'id envoyé en PathVariable
+     */
     @DeleteMapping("{id}")
     public void deleteById(@PathVariable String id) {
         clientService.deleteById(id);
